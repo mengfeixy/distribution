f9021419d01d34832b278061a1d4f983fbc61707
xiaoxiansheng <93795958@qq.com>
2018-11-16 17:05:28 +0800

    up

---
+++ src/views/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'login',
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
