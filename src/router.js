0a8b3a841267bbb6e0e4e3b1e0b58f8f4cd0d290
keter0202 <752761959@qq.com>
2018-11-21 14:45:09 +0800

    -添加stylus -添加keter - index

--- src/router.js
+++ src/router.js
@@ -3,6 +3,9 @@ import Router from 'vue-router'
 import test from '@/views/test'
 import Center from '@/views/personCenter'
 
+// keter
+import keter from '@/views/keter/index'
+
 Vue.use(Router)
 
 module.exports = new Router({
@@ -15,6 +18,10 @@ module.exports = new Router({
       path: '/personCenter',
       name: 'Center',
       component: Center
+    }, {
+      path: '/keter',
+      name: 'keter',
+      component: keter
     }
   ]
 })
---
+++ src/views/keter/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="keter">
+    <text class="test">test1231231</text>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'keter'
+}
+</script>
+
+<style lang="stylus" scoped>
+.keter
+  width: 100%
+  height: auto
+  .test
+    color: red
+</style>
