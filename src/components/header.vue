e22f5a0a37975c7a07fea1e4403fa87e1becc7dd
xiaoxiansheng <93795958@qq.com>
2018-11-21 16:29:52 +0800

    Header组件

---
+++ src/components/header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header-box">
+    <i class="iconfont return" @click="$router.back()"></i>
+    <text class="header-text" v-if="showText">{{title}}</text>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  props: {
+    title: '', // 标题
+    showText: Boolean // 是否显示title
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header-box
+    width: 100%
+    height: 88px
+    position: fixed;
+    z-index: 5000;
+    top: 0;
+    left: 0;
+
+  .header-text
+  .return
+    font-size: 36px
+    line-height: 88px
+
+  .header-text
+    color: rgba(102, 102, 102, 1)
+    text-align: center
+
+  .return
+    color: rgba(34, 34, 34, 1)
+    position: absolute
+    left: 28px
+    z-index: 1000
+</style>
--- src/views/personCenter/index.vue
+++ src/views/personCenter/index.vue
@@ -1,32 +1,41 @@
 <template>
   <div>
+    <Header title="个人中心" :showText="true"></Header>
     <text>个人中心</text>
   </div>
 </template>
 
 <script>
-let stream = weex.requireModule('stream')
+import Header from '@/components/header.vue'
+// let stream = weex.requireModule('stream')
 
 export default {
   name: 'index',
+  components: {
+    Header
+  },
   data () {
     return {
       qwer: ''
     }
   },
   created () {
-    stream.fetch({
-      method: 'post',
-      url: '/news/index',
-      type: 'json'
-    }, (res) => {
-      let data = JSON.parse(res.data)
-      console.log(data, '0000')
-    })
+    // stream.fetch({
+    //   method: 'post',
+    //   url: '/news/index',
+    //   type: 'json'
+    // }, (res) => {
+    //   let data = JSON.parse(res.data)
+    //   console.log(data, '0000')
+    // })
   }
 }
 </script>
 
 <style scoped>
-
+  .box {
+    width: 100%;
+    height: 424px;
+    background-color: black;
+  }
 </style>
