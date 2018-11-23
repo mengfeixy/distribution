f9aa1406379543338dc60d5ebcf64639235222d3
xiaoxiansheng <93795958@qq.com>
2018-11-23 18:53:45 +0800

    绑定申请(100%) 绑定手机号(100%)

--- src/views/personCenter/bindPhone.vue
+++ src/views/personCenter/bindPhone.vue
@@ -12,6 +12,20 @@
         @click="bindPhoneFun">
       确认
     </button>
+
+    <!-- 弹窗 -->
+    <div v-if="showPop">
+      <div class="pop-mask" @click="showPop = false"></div>
+      <div class="pop-content">
+        <div class="content-title">
+          <text style="font-size:32px;color: rgba(102, 102, 102, 1);margin-bottom: 20px;">手机号输入不正确</text>
+          <text>请重新输入</text>
+        </div>
+        <div class="confirm-btn">
+          <text @click="showPop = false">确认</text>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,16 +40,18 @@
     data () {
       return {
         bindPhone: '',
-        disabled: true
+        showPop: false // 是否显示弹窗
       }
-    },
-    created () {
-      console.log('bind')
     },
     methods: {
       bindPhoneFun () {
         if (this.bindPhone.length === 11) {
-          this.$router.push('/submitApply')
+          let phoneRge = /^1[3-9][0-9]{9}$/
+          if (phoneRge.test(this.bindPhone)) {
+            this.$router.replace('/submitApply')
+          } else {
+            this.showPop = true
+          }
         }
       }
     }
@@ -93,4 +109,50 @@
       outline none
     ::placeholder
       color rgba(153, 153, 153, 1)
+
+  .pop-mask
+    background rgba(74, 74, 74, 0.25)
+    position fixed
+    top 0
+    left 0
+    right 0
+    bottom 0
+    z-index 6000
+
+  .pop-content
+    width 66.7%
+    height 296px
+    background-color rgba(255, 255, 255, 1)
+    -moz-box-shadow 0 4px 8px 0 rgba(0, 0, 0, 0.1)
+    -webkit-box-shadow 0 4px 8px 0 rgba(0, 0, 0, 0.1)
+    box-shadow 0 4px 8px 0 rgba(0, 0, 0, 0.1)
+    border-radius 12px
+    position fixed
+    z-index 7000
+    top 50%
+    left 50%
+    -webkit-transform: translate(-50%, -50%)
+    -moz-transform: translate(-50%, -50%)
+    -ms-transform: translate(-50%, -50%)
+    -o-transform: translate(-50%, -50%)
+    transform: translate(-50%, -50%)
+
+  .content-title
+    height 200px
+    width 100%
+    border-bottom 2px solid rgba(205, 211, 215, 1)
+    flex-direction column
+    justify-content center
+
+  .content-title
+    p
+      font-size 28px
+      color rgba(153, 153, 153, 1)
+      text-align center
+
+  .confirm-btn
+    p
+      color rgba(34, 34, 34, 1)
+      text-align center
+      line-height 96px
 </style>
--- src/views/personCenter/submitApply.vue
+++ src/views/personCenter/submitApply.vue
@@ -3,9 +3,9 @@
     <Header title="提交申请"></Header>
     <div class="apply-content">
       <i class="iconfont checkbox-circle-chec"></i>
-      <text>绑定申请已提交，等待对方审核通过</text>
-      <text>可在“绑定审核”中查看进程</text>
-      <button>完成</button>
+      <text style="font-size: 32px;color: rgba(102, 102, 102, 1)">绑定申请已提交，等待对方审核通过</text>
+      <text style="font-size: 28px;color: rgba(153, 153, 153, 1);margin-top: 40px;">可在“绑定审核”中查看进程</text>
+      <button @click="$router.replace('/personCenter')">完成</button>
     </div>
   </div>
 </template>
@@ -27,5 +27,28 @@
 <style lang="stylus" scoped>
   .apply-content
     width 100%
-    height 100%
+    height 100vh
+    padding-top 198px
+    background rgba(255, 255, 255, 1)
+
+  .apply-content
+    i
+    p
+    button
+      margin 0 auto
+    i
+      font-size 100px
+      margin-bottom 98px
+    button
+      width 48%
+      height 96px
+      border 2px solid rgba(34, 34, 34, 1)
+      -webkit-border-radius: 96px
+      -moz-border-radius: 96px
+      border-radius: 96px
+      outline none
+      background rgba(255, 255, 255, 1)
+      color rgba(34, 34, 34, 1)
+      text-align center
+      margin-top 130px
 </style>
